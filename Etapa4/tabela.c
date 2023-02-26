7e1232dd55c63afb16722d058ec17be81f963e98
Bruna Gonzaga <bruna.gonzaga@ufrgs.br>
2023-02-26 12:35:09 -0300

    start tabela - so comentários

--- Etapa4/tabela.c
+++ Etapa4/tabela.c
@@ -1,1 +1,19 @@
 #include "tabela.h"
+
+/*
+    2.1: TABELA
+        - Chave é o lexema: pode ser obtido pelo valor_lexico
+        - tabela hash: ver especificação
+
+    2.2: VERIF DECLARAÇÕES
+        - pilha de tabelas: locais de push e pop
+        - como verificar se ja declarou
+
+    2.3: USO CORRETO
+        - encontrar na tabela
+            - descobre natureza
+            - match com situações na gramática
+
+    2.4: VERIFICACAO DE TIPO
+        - Campo novo na estrutura da arvore de tipo (4 valores possiveis)
+ */
